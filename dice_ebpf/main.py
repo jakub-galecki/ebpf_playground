750a89c002ce0c8136c23aa4c830abea021c74d8
jakub-galecki <galecki792@gmail.com>
2025-03-30 22:51:53 +0200

    x

--- dice_ebpf/main.py
+++ dice_ebpf/main.py
@@ -19,33 +19,58 @@ class bcolors:
 
 # print(bcolors.OKCYAN + "Reading for pid: " + target_pid + bcolors.ENDC)
 
+
+# __user - indicates that pointer is in the userspace
 prog = """
 #include <uapi/linux/ptrace.h>
+#include <linux/fs.h>
+#include <linux/uio.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
 
-int hello_world(void *ctx) {
-    bpf_trace_printk("Hello, World!\\n");
+#define BUF_LEN 128
+
+struct data_t {
+    u32 pid;
+    u64 ts;
+    char comm[TASK_COMM_LEN];
+    char buf[BUF_LEN];
+    int buf_len;
+};
+
+//PF_PERF_OUTPUT(events);
+
+
+int syscall__read(struct pt_regs *ctx, int fd, const char __user *buf, size_t count) {
+    struct data_t data = {};
+    u64 pid_tgid = bpf_get_current_pid_tgid();
+    data.pid = pid_tgid >> 32;
+
+    
+    char comm[TASK_COMM_LEN];
+    data.ts = bpf_ktime_get_ns();
+    bpf_probe_read_user_str(&data.comm, sizeof(data.comm), comm);
+
+    bpf_probe_read_user_str(&data.buf, sizeof(data.buf), buf);
+
+  //  events.perf_submit(ctx, &data, sizeof(data));
+    bpf_trace_printk("READ_SYSCALL: fd: <%d>, comm: <%s>\\n", fd, data.comm);
     return 0;
 }
 """
 
 b = BPF(text=prog)
-b.attach_kprobe(event=b.get_syscall_fnname("read"), fn_name="hello_world")
-b.attach_kprobe(event=b.get_syscall_fnname("write"), fn_name="hello_world")
+b.attach_kprobe(event=b.get_syscall_fnname("read"), fn_name="syscall__read")
+# b.attach_kprobe(event=b.get_syscall_fnname("write"), fn_name="hello_world")
 
-print("Tracing processes in the system... Ctrl-C to end")
-# header
+# def print_event(cpu, data, size):
+#     event = b["events"].event(data)
+#     print(f"PID: {event.pid}, Comm: {event.comm.decode('utf-8')}, Buffer: {event.buf[:event.buf_len].decode('utf-8', errors='replace')}")
 
-# header
-print("%-18s %-16s %-6s %s" % ("TIME(s)", "COMM", "PID", "MESSAGE"))
-
-# format output
-while 1:
+# b["events"].open_perf_buffer(print_event)
+while True:
     try:
         (task, pid, cpu, flags, ts, msg) = b.trace_fields()
-        # if pid != target_pid:
-        #     continue
-    except ValueError:
-        continue
     except KeyboardInterrupt:
         exit()
-    printb(b"%-18.9f %-16s %-6d %s" % (ts, task, pid, msg))
+    print(msg)
